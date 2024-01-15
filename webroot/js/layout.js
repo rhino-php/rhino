32733eae9077a763c99eeaa6b2bf82d2f114205c
Carsten Coull <carsten.coull@swu.de>
2024-01-15 17:20:19 +0100

    hotfix

--- webroot/js/layout.js
+++ webroot/js/layout.js
@@ -1,27 +1,9 @@
-import ComponentsHandler from "/rhino/js/modules/componentsHandler.js";
-// import EditorJS from "/rhino/js/vendor/editor.js";
-
-class Layout {
-	constructor() {
-		this.debug = false;
-		window.addEventListener("load", (event) => {
-			this.init();
-		});
-	}
-	
-	init() {
-		this.Components = new ComponentsHandler(this);
-		// this.elements.setModal(this.LayoutModal.modal);
-	}
-
-	getToken() {
-		return document.querySelector('meta[name="csrfToken"]').getAttribute('content');
-	}
-
-	getPageId() {
-		return document.querySelector('meta[name="pageId"]').getAttribute('content');
-	}
-}
-
-new Layout();
-//# sourceMappingURL=layout.js.map
+/**
+ * @project       tusk
+ * @author        carsten.coull@swu.de
+ * @build         Mon, Jan 15, 2024 5:20 PM ET
+ * @release       318425badc6f83015e6e4329f732d4a94c83c618 [dev]
+ * @copyright     Copyright (c) 2024, SWU Stadtwerke Ulm / Neu-Ulm GmbH
+ *
+ */
+import ComponentsHandler from"/rhino/js/modules/componentsHandler.js";class Layout{constructor(){this.debug=!1,window.addEventListener("load",(e=>{this.init()}))}init(){this.Components=new ComponentsHandler(this)}getToken(){return document.querySelector('meta[name="csrfToken"]').getAttribute("content")}getPageId(){return document.querySelector('meta[name="pageId"]').getAttribute("content")}}new Layout;
--- webroot/js/modules/hooks-handler.js
+++ webroot/js/modules/hooks-handler.js
@@ -1,32 +1,9 @@
-export default class HooksHandler {
-	constructor(main) {
-		this.main = main;
-
-		if (this.main.debug) {
-			console.debug("HooksHandler::const");
-		}
-
-		// (A1) CURRENT HOOK QUEUE
-		this.queue = {};
-	}
-
-	// (A2) ADD FUNCTION TO QUEUE
-	// name : name of function to add hook to
-	// fn : function to call
-	add(name, fn) {
-		if (!this.queue[name]) { this.queue[name] = []; }
-		this.queue[name].push(fn);
-	}
-
-	// (A3) CALL A HOOK
-	// name : name of function to add hook to
-	// params : parameters
-	call(name, ...params) {
-		console.log(name);
-		if (this.queue[name]) {
-			this.queue[name].forEach((fn) => { fn(...params); });
-			delete this.queue[name];
-		}
-	}
-}
-//# sourceMappingURL=hooks-handler.js.map
+/**
+ * @project       tusk
+ * @author        carsten.coull@swu.de
+ * @build         Mon, Jan 15, 2024 5:20 PM ET
+ * @release       318425badc6f83015e6e4329f732d4a94c83c618 [dev]
+ * @copyright     Copyright (c) 2024, SWU Stadtwerke Ulm / Neu-Ulm GmbH
+ *
+ */
+export default class HooksHandler{constructor(e){this.main=e,this.main.debug,this.queue={}}add(e,u){this.queue[e]||(this.queue[e]=[]),this.queue[e].push(u)}call(e,...u){this.queue[e]&&(this.queue[e].forEach((e=>{e(...u)})),delete this.queue[e])}}
