6ce7d4c57cb50cb9315e7d5a7ba01fd8bdeac8c1
Tyqo <carsten.coull@hotmail.de>
2024-10-22 14:32:35 +0200

    moved auth and stuff to RhinoPlugin.php

--- src/RhinoPlugin.php
+++ src/RhinoPlugin.php
@@ -10,12 +10,31 @@ use Cake\Core\PluginApplicationInterface;
 use Cake\Http\MiddlewareQueue;
 use Cake\Routing\RouteBuilder;
 
+use Authentication\AuthenticationService;
+use Authentication\AuthenticationServiceInterface;
+use Authentication\AuthenticationServiceProviderInterface;
+use Authentication\Middleware\AuthenticationMiddleware;
+use Cake\Routing\Router;
+use Psr\Http\Message\ServerRequestInterface;
+
+use Authorization\AuthorizationService;
+use Authorization\AuthorizationServiceInterface;
+use Authorization\AuthorizationServiceProviderInterface;
 use Authorization\Middleware\AuthorizationMiddleware;
+use Authorization\Middleware\RequestAuthorizationMiddleware;
+use Authorization\Policy\ResolverCollection;
+use Authorization\Policy\OrmResolver;
+use Authorization\Policy\MapResolver;
+use Authorization\Exception\ForbiddenException;
+use Authentication\Identifier\AbstractIdentifier;
+
+use Cake\Http\ServerRequest;
+use App\Policy\RequestPolicy;
 
 /**
  * Plugin for Rhino
  */
-class RhinoPlugin extends BasePlugin {
+class RhinoPlugin extends BasePlugin implements AuthenticationServiceProviderInterface, AuthorizationServiceProviderInterface {
 	/**
 	 * Load all the plugin configuration and bootstrap logic.
 	 *
@@ -64,7 +83,20 @@ class RhinoPlugin extends BasePlugin {
 	 * @return \Cake\Http\MiddlewareQueue
 	 */
 	public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue {
-		return $middlewareQueue;
+		return $middlewareQueue
+			->add(new AuthenticationMiddleware($this))
+			->add(new AuthorizationMiddleware($this, [
+				'unauthorizedHandler' => [
+					'className' => 'Authorization.Redirect',
+					'url' => '/rhino/users/login',
+					'queryParam' => 'redirectUrl',
+					'exceptions' => [
+						MissingIdentityException::class,
+						ForbiddenException::class,
+					],
+				],
+			]))
+			->add(new RequestAuthorizationMiddleware());
 	}
 
 	/**
@@ -90,5 +122,76 @@ class RhinoPlugin extends BasePlugin {
 	 */
 	public function services(ContainerInterface $container): void {
 		// Add your services here
+	}
+
+	public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface {
+		if ($request->getParam('plugin') === "Rhino" || $request->getParam('prefix') == 'RhinoApp') {
+			// Reuse fields in multiple authenticators.
+			$fields = [
+				AbstractIdentifier::CREDENTIAL_USERNAME => 'email',
+				AbstractIdentifier::CREDENTIAL_PASSWORD => 'password',
+			];
+
+			$login = Router::url([
+				'plugin' => 'Rhino',
+				'controller' => 'Users',
+				'action' => 'login',
+				'prefix' => false,
+			]);
+
+			$authenticationService = new AuthenticationService([
+				'unauthenticatedRedirect' => $login,
+				'queryParam' => 'redirect',
+			]);
+
+			// Load the authenticators, you want session first
+			$authenticationService->loadAuthenticator('Authentication.Session');
+
+			// If the user is on the login page, check for a cookie as well.
+			$authenticationService->loadAuthenticator('Authentication.Cookie', [
+				'fields' => $fields
+			]);
+
+			// Configure form data check to pick email and password
+			$authenticationService->loadAuthenticator('Authentication.Form', [
+				'fields' => $fields
+			]);
+
+			// Load identifiers, ensure we check email and password fields
+			$authenticationService->loadIdentifier('Authentication.Password', [
+				'fields' => $fields,
+				'resolver' => [
+					'className' => 'Authentication.Orm',
+					'userModel' => 'Rhino.Users',
+					'finder' => 'all', // alterenatively: 'active'
+				]
+			]);
+
+			return $authenticationService;
+		}
+
+		$authenticationService = new AuthenticationService();
+		// Load identifiers, ensure we check email and password fields
+		$authenticationService->loadIdentifier('Authentication.Password');
+		// Load the authenticators, you want session first
+		$authenticationService->loadAuthenticator('Authentication.Session');
+
+		return $authenticationService;
+	}
+
+	public function getAuthorizationService(ServerRequestInterface $request): AuthorizationServiceInterface {
+		if ($request->getParam('plugin') === "Rhino") {
+			$ormResolver = new OrmResolver();
+			$mapResolver = new MapResolver();
+			$mapResolver->map(ServerRequest::class, RequestPolicy::class);
+			// $resolver = new ResolverCollection([$mapResolver, $ormResolver]);
+			return new AuthorizationService($mapResolver);
+		}
+
+		// $ormResolver = new OrmResolver();
+		$mapResolver = new MapResolver();
+		$mapResolver->map(ServerRequest::class, RequestPolicy::class);
+		// $resolver = new ResolverCollection([$mapResolver, $ormResolver]);
+		return new AuthorizationService($mapResolver);
 	}
 }
